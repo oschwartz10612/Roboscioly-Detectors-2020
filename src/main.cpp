b212687f55ae1551c4bdadab6d3e486df3a44193
Owen Schwartz <oschwartz10612@gmail.com>
2019-12-06 20:24:54 -0500

    NCS

--- src/main.cpp
+++ src/main.cpp
@@ -26,16 +26,13 @@ ADC *adc = new ADC(); // adc object
 #define TEMPERATURENOMINAL 25   
 #define NUMSAMPLES 5
 #define SERIESRESISTOR 10000
-#define BCOEFFICIENT 
 
-#define A 1.139
-#define B 2.324
-#define C 0.938
+#define M 0.001581
+#define B 3.963
  
 int samples[NUMSAMPLES];
  
-void setup(void) 
-{
+void setup(void) {
 	Serial.begin(9600);
 	adc->setReference(ADC_REFERENCE::REF_EXT, ADC_0);
 	adc->setResolution(16);
@@ -50,23 +47,17 @@ void setup(void)
 	digitalWrite(BLUE, LOW);
 }
  
-void loop(void) 
-{
+void loop(void) {
 	uint8_t i;
 	float average;
  
-	/* Read the value outputted by the thermistor and put them into the samples array
-	 * One sample per ten miliseconds*/
-	for (i=0; i< NUMSAMPLES; i++) 
-	{
+	for (i=0; i< NUMSAMPLES; i++) {
 		samples[i] = adc->analogRead(THERMISTORPIN, ADC_0);
 		delay(10);
 	}
   
-	// Determine the average of the array samples
 	average = 0;
-	for (i=0; i< NUMSAMPLES; i++) 
-	{
+	for (i=0; i< NUMSAMPLES; i++) {
 		average += samples[i];
 	}
   
@@ -74,50 +65,38 @@ void loop(void)
 
 	Serial.print("Avarage ADC "); 
 	Serial.println(average);
- 
-	// Convert analog data value to resistance
 
 	float R;
 	R = (adc->getMaxValue(ADC_0) / average)-1;
 	R = SERIESRESISTOR / R;
 	Serial.print("Thermistor resistance "); 
-	Serial.println(average);
- 	
-	// Temperature calculation via expanded steinhart equation
-        // May not work as of 12.6.2019 15:03
-		
-	float steinhart;
-	steinhart = 1/(A+(B*log(R))+pow((C*log(R), 3)))
+	Serial.println(R);
 
-	// Simplified steinhart equation
-	
-	float simp_steinhart;
-	simp_steinhart = average / THERMISTORNOMINAL;		// (R/Ro)
-	simp_steinhart = log(simp_steinhart);			// ln(R/Ro)
-	simp_steinhart /= BCOEFFICIENT;				// 1/B * ln(R/Ro)
-	simp_steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15);	// + (1/To)
-	simp_steinhart = 1.0 / simp_steinhart;			// Invert
-	simp_steinhart -= 273.15;      
+  float voltage;
+  voltage = average * (4.0960/adc->getMaxValue(ADC_0));
+
+  Serial.print("Voltage "); 
+  Serial.println(voltage);
+ 
+  float temp;
+  //y=mx+b
+  temp = (M*average);
+  temp += B;
 
 	Serial.print("Temperature "); 
-	Serial.print(simp_steinhart);
+	Serial.print(temp);
 	Serial.println(" *C");
 
 	// RED
-	if (steinhart < UPPERRED && steinhart > LOWERRED) 
-	{
+	if (temp < UPPERRED && temp > LOWERRED) {
 		digitalWrite(RED, HIGH);
 	}
-
 	// GREEN
-	if (steinhart < UPPERGREEN && steinhart > LOWERGREEN) 
-	{
+	if (temp < UPPERGREEN && temp > LOWERGREEN) {
 		digitalWrite(GREEN, HIGH);
 	}
-  
 	// BLUE
-	if (steinhart < UPPERBLUE && steinhart > LOWERBLUE) 
-	{
+	if (temp < UPPERBLUE && temp > LOWERBLUE) {
 		digitalWrite(BLUE, HIGH);
 	}
 
@@ -125,7 +104,3 @@ void loop(void)
   
 	delay(1000);
 }
-
-// Adapted from Thermistor Example #3 from the Adafruit Learning System guide on Thermistors 
-// https://learn.adafruit.com/thermistor/overview by Limor Fried, Adafruit Industries
-// MIT License - please keep attribution and consider buying parts from Adafruit
