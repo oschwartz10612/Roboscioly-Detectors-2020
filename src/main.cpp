7c136f0dfa6515a00c74ea57cc5173ff424f0222
Owen Schwartz <oschwartz10612@gmail.com>
2019-12-06 22:04:23 -0500

    DONE

--- src/main.cpp
+++ src/main.cpp
@@ -14,21 +14,18 @@ ADC *adc = new ADC(); // adc object
 #define LOWERBLUE 30
 #define UPPERBLUE 40
 
-#define RED 2
-#define GREEN 3
-#define BLUE 4
+#define BLUE 2
+#define RED 3
+#define GREEN 4
 
 //SENSOR
-#define OFFSET 7
+#define OFFSET 18;
 
 #define THERMISTORPIN A0         
 #define THERMISTORNOMINAL 10000      
 #define TEMPERATURENOMINAL 25   
-#define NUMSAMPLES 5
+#define NUMSAMPLES 50
 #define SERIESRESISTOR 10000
-
-#define M 0.001581
-#define B 3.963
  
 int samples[NUMSAMPLES];
  
@@ -48,59 +45,86 @@ void setup(void) {
 }
  
 void loop(void) {
-	uint8_t i;
-	float average;
- 
-	for (i=0; i< NUMSAMPLES; i++) {
-		samples[i] = adc->analogRead(THERMISTORPIN, ADC_0);
-		delay(10);
-	}
+  if (Serial.available() > 0) {
+    char t = Serial.read();
+    Serial.flush();
+
+    digitalWrite(RED, LOW);
+	  digitalWrite(GREEN, LOW);
+	  digitalWrite(BLUE, LOW);
+
+    uint8_t i;
+    float average;
   
-	average = 0;
-	for (i=0; i< NUMSAMPLES; i++) {
-		average += samples[i];
-	}
+    for (i=0; i< NUMSAMPLES; i++) {
+      samples[i] = adc->analogRead(THERMISTORPIN, ADC_0);
+      delay(10);
+    }
+    
+    average = 0;
+    for (i=0; i< NUMSAMPLES; i++) {
+      average += samples[i];
+    }
+    
+    average /= NUMSAMPLES;
+
+    Serial.print("Avarage ADC: "); 
+    Serial.println(average);
+
+    float R;
+    R = (adc->getMaxValue(ADC_0) / average)-1;
+    R = SERIESRESISTOR / R;
+    Serial.print("Thermistor resistance: "); 
+    Serial.println(R);
+
+    float voltage;
+    voltage = average * (4.0960/adc->getMaxValue(ADC_0));
+
+    Serial.print("Voltage "); 
+    Serial.println(voltage);
   
-	average /= NUMSAMPLES;
+    float temp;
+    //y=mx+b
+    temp = 626.5 * voltage;
+    temp = temp - 2482;
+    temp = temp + OFFSET;
 
-	Serial.print("Avarage ADC "); 
-	Serial.println(average);
+    Serial.print("Temperature from voltage EQ: "); 
+    Serial.print(temp);
+    Serial.println(" *C");
 
-	float R;
-	R = (adc->getMaxValue(ADC_0) / average)-1;
-	R = SERIESRESISTOR / R;
-	Serial.print("Thermistor resistance "); 
-	Serial.println(R);
+    //T=-104.9+0.0004889R-(5.13*10-10)R2+(1.85*10-16)R3
+    float tempVal;
 
-  float voltage;
-  voltage = average * (4.0960/adc->getMaxValue(ADC_0));
+    temp = 0.0004889 * R;
+    tempVal = 5.13e-10 * pow(R, 2);
+    temp = temp - tempVal;
+    tempVal = 1.85e-16 * pow(R, 3);
+    temp = temp + tempVal;
+    temp = temp - 104.9;
+    temp = temp + OFFSET;
 
-  Serial.print("Voltage "); 
-  Serial.println(voltage);
- 
-  float temp;
-  //y=mx+b
-  temp = (M*average);
-  temp += B;
+    Serial.print("Temperature from resistance EQ: "); 
+    Serial.print(temp);
+    Serial.println(" *C");
 
-	Serial.print("Temperature "); 
-	Serial.print(temp);
-	Serial.println(" *C");
+    // RED
+    // if (temp < UPPERRED && temp > LOWERRED) {
+    //   Serial.println("RED");
+    //   digitalWrite(RED, HIGH);
+    // }
+    // // GREEN
+    // if (temp < UPPERGREEN && temp > LOWERGREEN) {
+    //   Serial.println("GREEN");
+    //   digitalWrite(GREEN, HIGH);
+    // }
+    // // BLUE
+    // if (temp < UPPERBLUE && temp > LOWERBLUE) {
+    //   Serial.println("BLUE");
+    //   digitalWrite(BLUE, HIGH);
+    // }
 
-	// RED
-	if (temp < UPPERRED && temp > LOWERRED) {
-		digitalWrite(RED, HIGH);
-	}
-	// GREEN
-	if (temp < UPPERGREEN && temp > LOWERGREEN) {
-		digitalWrite(GREEN, HIGH);
-	}
-	// BLUE
-	if (temp < UPPERBLUE && temp > LOWERBLUE) {
-		digitalWrite(BLUE, HIGH);
-	}
-
-	adc->printError();
-  
-	delay(1000);
+    adc->printError();
+  }
+  delay(100);
 }
