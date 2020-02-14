b176404c7f159c92f93b17243aaba89e0b8bff2c
Stefan Arseneau <sarseneau@localhost.localdomain>
2020-02-14 15:21:28 -0500

    Wibbly wobbily woo, I wrote some code for you!

--- src/main.cpp
+++ src/main.cpp
@@ -5,35 +5,34 @@
 ADC *adc = new ADC(); // adc object
 
 //LEDS
-#define LOWERRED 55
-#define UPPERRED 500000
+#define LOWERRED 0
+#define UPPERRED 10
 
-#define LOWERGREEN 20
-#define UPPERGREEN 50
+#define LOWERGREEN 10
+#define UPPERGREEN 30
 
-#define LOWERBLUE 0
-#define UPPERBLUE 20
+#define LOWERBLUE 30
+#define UPPERBLUE 100
 
 #define RED 3
-#define GREEN 5
-#define BLUE 7
+#define GREEN 2
+#define BLUE 4
 
 //SENSOR
 #define OFFSET 0;
 #define OFFSET2 0;
 
-#define THERMISTORPIN A0
+#define THERMISTORPIN A0         
 #define NUMSAMPLES 50
-#define VCONSTANT 4.096/adc->getMaxValue(ADC_0)
-
+#define VCONSTANT 3.3/adc->getMaxValue(ADC_0)
+ 
 int samples[NUMSAMPLES];
 float prev_voltage = 1000;
-
 
 void setup(void) {
 	Serial.begin(9600);
   adc->setResolution(16);
-  adc->setReference(ADC_REFERENCE::REF_EXT, ADC_0);
+ // adc->setReference(ADC_REFERENCE::REF_EXT, ADC_0);
 
 
 	pinMode(RED, OUTPUT); //RED
@@ -45,12 +44,12 @@ void setup(void) {
 	digitalWrite(GREEN, LOW);
 	digitalWrite(BLUE, LOW);
 }
-
+ 
 void loop(void) {
 
     uint8_t i;
     float average;
-
+  
     for (i=0; i< NUMSAMPLES; i++) {
       samples[i] = adc->analogRead(THERMISTORPIN, ADC_0);
       delay(10);
@@ -60,10 +59,10 @@ void loop(void) {
     for (i=0; i< NUMSAMPLES; i++) {
       average += samples[i];
     }
-
+  
     average /= NUMSAMPLES;
 
-    // Serial.print("Average ADC: ");
+    // Serial.print("Average ADC: "); 
     // Serial.println(average);
 
     float voltage;
@@ -72,34 +71,34 @@ void loop(void) {
     float delta_voltage;
     delta_voltage = abs(voltage - prev_voltage);
 
-    //Temperature function T(v) = (4.506)v^2-(2.392)v-(0.007079)
-
-    if(delta_voltage < 0.03)
+    if(delta_voltage < 0.02)
     {
       float temperature;
-      temperature = ((3.746) * exp(0.8312 * voltage) - (11.30));
+      temperature = (27.62) * Math.e((0.4740) * voltage) + (-36.47);
 
       Serial.println("");
       Serial.println("System settled");
-      Serial.print("Temperature: ");
+      Serial.print("Voltage: ");
+      Serial.println(voltage, 5);
+      Serial.print("Temperature: "); 
       Serial.println(temperature);
 
       //RED
-      if (temperature < UPPERRED && temperature > LOWERRED) {
+      if (temperature <= UPPERRED && temperature >= LOWERRED) {
         Serial.println("RED");
         digitalWrite(RED, HIGH);
         digitalWrite(GREEN, LOW);
         digitalWrite(BLUE, LOW);
       }
       // GREEN
-      if (temperature < UPPERGREEN && temperature > LOWERGREEN) {
+      if (temperature <= UPPERGREEN && temperature >= LOWERGREEN) {
         Serial.println("GREEN");
         digitalWrite(GREEN, HIGH);
         digitalWrite(RED, LOW);
         digitalWrite(BLUE, LOW);
       }
       // BLUE
-      if (temperature < UPPERBLUE && temperature > LOWERBLUE) {
+      if (temperature < UPPERBLUE && temperature >= LOWERBLUE) {
         Serial.println("BLUE");
         digitalWrite(BLUE, HIGH);
         digitalWrite(RED, LOW);
@@ -110,8 +109,8 @@ void loop(void) {
     {
       Serial.print(".");
     }
-
-    prev_voltage = voltage;
+  
+    prev_voltage = voltage;    
 
   delay(500);
 }
